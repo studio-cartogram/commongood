a060993ed91035e7ddad34baf3a8efec85e6741f
Matthew Seccafien <matt@studiocartogram.com>
2013-12-16 21:53:48 -0500

    removed unneccesary bits
    Signed-off-by: Matthew Seccafien <matt@studiocartogram.com>

--- app/themes/truefriend/page.php
+++ app/themes/truefriend/page.php
@@ -1,32 +1,24 @@
 <?php
 
 /*
-Template Name: About
+Template Name: Page
 */
-
-get_template_part('partials/head');
+get_template_part('part-head');
 get_header();
 
-global $title;
-global $sub_title;
-global $bannerImageSize;
 
 echo '<div class="js-site site">';
 	if (have_posts()) :
 		while (have_posts()) : the_post();
-			$slug = $post->post_name;
-			$title = get_the_title();
-			$sub_title = get_field("sub_title");
 			
-			get_template_part('part-mast', $slug);
-			get_template_part('part-islands');
 
 		endwhile; // endwhile have posts
 	else :
 		get_template_part('part-not-found');
 	endif;
-get_footer();
 echo '</div>';
-get_template_part('part-drawer');
-get_template_part('partials/foot');
+
+
+get_footer();
+get_template_part('part-foot');
 ?>
