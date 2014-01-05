bd5aa0dd641240ebd635f83143e205c4f78fb714
Matthew Seccafien <matt@studiocartogram.com>
2014-01-04 23:36:06 -0500

    removed auto play
    Signed-off-by: Matthew Seccafien <matt@studiocartogram.com>

--- app/themes/commongood/scripts/services/player.js
+++ app/themes/commongood/scripts/services/player.js
@@ -3,6 +3,6 @@
 angular.module('ngCommongoodApp')
 .factory('player', function ($sanitize, $sce) {
 	return function(videoId) {
-		return	$sce.trustAsResourceUrl( 'http://player.vimeo.com/video/' + videoId + '?autoplay=1&autopause=1&title=0&byline=0&badge=0&portrait=0');
+		return	$sce.trustAsResourceUrl( 'http://player.vimeo.com/video/' + videoId + '?autopause=1&title=0&byline=0&badge=0&portrait=0');
 	};			
 });
