ba13188a3a3e81bb5357606dedc7ea78b5158668
Matthew Seccafien <matt@studiocartogram.com>
2014-01-08 20:02:42 -0500

    added text on sides
    Signed-off-by: Matthew Seccafien <matt@studiocartogram.com>

--- app/themes/commongood/footer.php
+++ app/themes/commongood/footer.php
@@ -10,9 +10,8 @@ echo '<div id="footer" role="contentinfo" class="row row--dashed ">';
 	echo '<div class="grid soft--top">';
 		echo '<div class="grid__item nine-tenths">';
 			echo '<ul class="nav milli">';
-				if (is_page('contact')) { 
-					echo '<li class="site-title site-title-left"> <a href="' . get_bloginfo('url') . '" title="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '" rel="home">' . get_bloginfo( 'name' ) . '</a></li>'; 
-				} else { 
+				
+				if (!is_page('contact')) {  
 			    	echo '<li class="soft--right text--medium ">' . '837 Dundas Street West'  .  '</li>';
 			    	echo '<li class="soft--right text--medium ">' . 'Toronto, ON M6J 1V4' . '</li>';
 			    	echo '<li class="soft--right text--medium ">' . '+1 416 366 2552' . '</li>';
