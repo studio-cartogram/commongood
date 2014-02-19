fb6da4f210b6c5dc341870b4c9990b7df7e2ddf2
Fiona McDougall <fiona@studiocartogram.com>
2014-02-19 16:36:59 -0500

    testing values in the go tags

--- app/themes/commongood/part-head.php
+++ app/themes/commongood/part-head.php
@@ -14,8 +14,8 @@
 	<meta property="fb:app_id" content="729898417042523" />
 	<meta property="og:site_name" content="Common Good" />
 	<meta property="og:title" content="Common Good" />
-	<meta property="og:url" content="http://www.commongood.tv/{{player}}" />
-	<link rel="canonical" href="http://www.commongood.tv" />
+	<meta property="og:url" content="http://www.commongood.tv/" />
+	<link rel="canonical" href="{{videoId}}" />
 	<meta property="og:type" content="website" />
 	<meta property="og:description" content="A Canadian Directing Collective." />
 	<meta property="og:image" content="http://www.commongood.tv/app/uploads/2014/02/logo-wide.png" />
