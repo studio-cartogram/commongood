60ce93e4c5ef99f5656a73040ced5b3f80984265
Matt Seccafien <matt@studiocartogram.com>
2017-03-02 20:01:30 +0700

    svgs are loading

--- app/themes/commongoodv2/theme/index.php
+++ app/themes/commongoodv2/theme/index.php
@@ -12,9 +12,9 @@ echo '<div id="barba-wrapper">';
 
   echo '<main id="main" role="main" class="main">';
 
-  echo '<svg class="logo__icon icon"><use xlink:href="#logo"></use></svg>';
+  echo '<svg class=""><use xlink:href="#icon-logo-icon"></use></svg>';
 
-  get_template_part('partials/loop');
+  // get_template_part('partials/loop');
 
   echo '</main>';
 
