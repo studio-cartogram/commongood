8e4d3cbea4e69571573ce985fdf18920b6ae5f42
Matt Seccafien <matt@studiocartogram.com>
2017-03-02 14:15:04 +0700

    started sage based theme

---
+++ app/themes/cg/templates/layouts/base.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html @php(language_attributes())>
+  @include('partials.head')
+  <body @php(body_class())>
+    @php(do_action('get_header'))
+    @include('partials.header')
+    <div class="wrap container" role="document">
+      <div id="barba-wrapper">
+        <div class="barba-container">
+          <main class="main">
+            @yield('content')
+          </main>
+        </div>
+      </div>
+        @if (App\display_sidebar())
+          <aside class="sidebar">
+            @include('partials.sidebar')
+          </aside>
+        @endif
+      </div>
+    </div>
+    @php(do_action('get_footer'))
+    @include('partials.footer')
+    @php(wp_footer())
+  </body>
+</html>
