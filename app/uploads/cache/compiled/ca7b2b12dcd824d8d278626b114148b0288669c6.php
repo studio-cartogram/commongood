c8e2219c9487ce0ad6df6c8f7e5fa493ec9833c9
Matt Seccafien <matt@studiocartogram.com>
2017-03-05 18:24:19 +0700

    work

---
+++ app/uploads/cache/compiled/ca7b2b12dcd824d8d278626b114148b0288669c6.php
@@ -0,0 +1,16 @@
+<div class="header">
+  <div class="header__group header__group--reverse">
+    <div class="spinner">
+      <span class="loading open-circle"></span>
+    </div>
+    <div id="js-toggle--nav" class="hamburger nav-toggle">
+      <span class="line"></span>
+      <span class="line"></span>
+      <span class="line"></span>
+    </div>
+  </div>
+  <div class="header__group">
+    <a class="brand" href="<?php echo e(home_url('/')); ?>"><?php echo e(get_bloginfo('name', 'display')); ?></a>
+    <span class="header__item spinner">Loading</span>
+  </div>
+</div>
---
+++ app/uploads/cache/compiled/d440b46bc77a44c9fdc82d57cfa9d300431a8271.php
@@ -0,0 +1,8 @@
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <script src="https://use.typekit.net/ieh7owy.js"></script>
+  <script>try{Typekit.load({ async: true });}catch(e){}</script>
+  <?php (wp_head()); ?>
+</head>
