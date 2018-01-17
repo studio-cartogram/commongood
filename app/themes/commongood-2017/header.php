b5feb9ffd97cdd6f716f36ad44b53d6860175edb
feeohnah <fiona@studiocartogram.com>
2018-01-17 18:28:13 -0500

    adjusted link arrangement of items in header to accomodate a third, adjusted static and work layouts to accomodate this switch

--- app/themes/commongood-2017/header.php
+++ app/themes/commongood-2017/header.php
@@ -4,25 +4,7 @@ global $language;
 
 echo '<header role="banner" class="header">';
 
-  echo '<div class="header__group ">';
-
-    echo '<div class="js-nav-toggle hamburger nav-toggle">';
-
-      echo '<span class="line"></span>';
-      echo '<span class="line"></span>';
-      echo '<span class="line"></span>';
-
-    echo '</div>';
-
-    echo '<div class="spinner">';
-
-      echo '<span class="loading open-circle"></span>';
-
-    echo '</div>';
-
-  echo '</div>';
-
-  echo '<div class="header__group header__group--right">';
+  echo '<div class="header__group">';
 
     echo '<a href="' . get_bloginfo('url') . '" class="logo">';
 
@@ -37,6 +19,38 @@ echo '<header role="banner" class="header">';
       echo '<span class="header__item spinner spinner--right">';
       // Loading
       echo '</span>';
+
+  echo '</div>';
+
+  echo '<div class="header__group header__group--center">';
+
+  echo '<a href="' . get_bloginfo('url') . '/commongood">';
+
+    echo '<span class="gamma inline-block header__item--text">@commongood</span>';
+
+  echo '</a>';
+
+  echo '<span class="header__item spinner spinner--right">';
+    // Loading
+  echo '</span>';
+
+  echo '</div>';
+
+  echo '<div class="header__group header__group--right">';
+
+    echo '<div class="js-nav-toggle hamburger nav-toggle">';
+
+      echo '<span class="line"></span>';
+      echo '<span class="line"></span>';
+      echo '<span class="line"></span>';
+
+    echo '</div>';
+
+    echo '<div class="spinner">';
+
+      echo '<span class="loading open-circle"></span>';
+
+    echo '</div>';
 
   echo '</div>';
 
