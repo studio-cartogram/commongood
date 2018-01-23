012b30b90bbeeb7cf85ee5f93d6f0545fc95d895
feeohnah <fiona@studiocartogram.com>
2018-01-23 15:41:04 -0500

    created basic layout for single directors page even though we don’t really need this anymore

---
+++ app/themes/commongood-2017/partials/work-related-director.php
@@ -0,0 +1,63 @@
+<?php
+
+$directorWorks = get_posts(array(
+  'post_type' => 'works',
+  'meta_query' => array(
+    array(
+      'key' => 'talent', 
+      'value' => '"' . get_the_ID() . '"', 
+      'compare' => 'LIKE'
+    )
+  )
+));
+
+if( $directorWorks ):
+
+  echo '<div id="all-work" class="row row--full thumbnails js-videos">';
+
+  foreach( $directorWorks as $work):
+
+    $title = get_the_title($work->ID);
+    $client = get_field('client', $work->ID );
+    $vimeo_id = get_field('vimeo_id', $work->ID);
+    $thumbnail = get_field('thumbnail', $work->ID);
+
+    echo '<a href="' . get_permalink($work->ID) . '" class="thumbnail column">';
+
+      echo '<div
+        style="padding-bottom: 56%;"
+        class="ratio-container"
+      >';
+
+        echo '<img
+          data-vimeo-id="' . $vimeo_id . '"
+          data-object-fit="cover"
+          class="js-load-vimeo-image thumbnail__img "
+          alt="' . get_the_title($work->ID) . '"
+        />';
+
+        echo '<img
+          data-object-fit="cover"
+          class="thumbnail__img thumbnail__img--fallback"
+          alt="' . get_the_title($work->ID) . '"
+          src="' . $thumbnail . '"
+        />';
+
+      echo '</div>';
+
+      echo '<div class="thumbnail__copy">';
+
+        if ($client) : echo '<span class="gamma inline-block">' . $client . '</span> '; endif;
+
+        echo '<span class="gamma inline-block font-weight-regular">' . $title . '</span>';
+
+      echo '</div>';
+
+      echo '<div class="overlay thumbnail__overlay"></div>';
+
+    echo '</a>';
+
+  endforeach;
+
+endif;
+?>
--- app/themes/commongood-dev/theme/single-commongoods.php
+++ app/themes/commongood-dev/theme/single-commongoods.php
@@ -1,8 +1,7 @@
 <?php
 
-$reel = get_field('reel');
 $title = get_the_title();
-$works = get_field('director_works');
+$siteUrl = get_site_url();
 
 get_template_part('partials/head');
 
@@ -16,94 +15,27 @@ echo '<main id="main" role="main" class="main">';
 
 echo '<div id="barba-wrapper">';
 
-  echo '<div class="barba-container" data-namespace="works">';
+  echo '<div class="barba-container" data-namespace="single">';
+
+  while ( have_posts() ) : the_post();
 
     echo '<div class="work">';
 
-      echo '<section class="row work__header">';
+      echo '<div class="work__header">';
 
-        echo '<span class="alpha inline-block ">' . $title . '</span>';
+        echo '<span class="alpha inline-block">' . $title . '</span>';
 
-      echo '</section>';
-
-      get_template_part('partials/work-header');
-
-      echo '<div class="work__video">';
-
-        echo '<div style="padding-bottom: 56%;" class="ratio-container">';
-
-          echo '<iframe src="https://player.vimeo.com/video/' . $reel . '?autoplay=1&title=0&color=252328&portrait=0&byline=0"" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
-
-        echo '</div>';
+        echo '<a href="'. $siteUrl .'/commongood" class="epsilon inline-block">';
+        
+        echo 'All Directors</a>';
 
       echo '</div>';
 
-      get_template_part('partials/work-prev-next');
-
-      get_template_part('partials/work-footer');
+      get_template_part('partials/work-related-director');
 
     echo '</div>';
 
-
-      if( $works ):
-
-        echo '<div id="all-work" class="row row--full thumbnails js-videos">';
-        
-          foreach( $works as $post):
-
-          setup_postdata($post);
-
-            // clean this up
-
-            $title = get_the_title();
-            $client = get_field('client');
-            $vimeo_id = get_field('vimeo_id');
-            $thumbnail = get_field('thumbnail');
-
-            echo '<a href="' . get_permalink() . '" class="thumbnail column">';
-
-              echo '<div
-                style="padding-bottom: 56%;"
-                class="ratio-container"
-              >';
-
-                echo '<img
-                  data-vimeo-id="' . $vimeo_id . '"
-                  data-object-fit="cover"
-                  class="js-load-vimeo-image thumbnail__img "
-                  alt="' . esc_attr(get_the_title()) . '"
-                />';
-
-                echo '<img
-                  data-object-fit="cover"
-                  class="thumbnail__img thumbnail__img--fallback"
-                  alt="' . esc_attr(get_the_title()) . '"
-                  src="' . $thumbnail . '"
-                />';
-
-              echo '</div>';
-
-              echo '<div class="thumbnail__copy">';
-
-                if ($client) : echo '<span class="gamma inline-block">' . $client . '</span> '; endif;
-
-                echo '<span class="gamma inline-block font-weight-regular">' . $title . '</span>';
-
-              echo '</div>';
-
-              echo '<div class="overlay thumbnail__overlay"></div>';
-
-            echo '</a>';
-
-            // end clean this up
-
-            endforeach;
-
-            wp_reset_postdata();
-        
-        echo '</div>';
-
-      endif;
+  endwhile;
 
   echo '</div>';
 
@@ -116,3 +48,5 @@ get_footer();
 get_template_part('partials/curtain');
 
 get_template_part('partials/foot');
+
+?>
